812904ab54a89abcf31a3783616031477b127b0b
Christopher Rose <crose@hcsdoh.org>
2017-10-18 16:38:02 -0400

    Added refresh results button

--- results_summary.php
+++ results_summary.php
@@ -67,7 +67,7 @@
 		echo "</div>";
 		
 		echo "<div class='row' id='reloadbutton' style='margin-top:-40px; margin-left:5px;'>";
-				echo "<button class='modal-action waves-effect btn-flat white-text' id='reload' style='margin-left:5px; background-color:"; echo sitesettings("sitecolor"); echo "'>Reload</button>";
+				echo "<button class='modal-action waves-effect btn-flat white-text' id='reload' style='margin-left:5px; background-color:"; echo sitesettings("sitecolor"); echo "'>Refresh Results</button>";
 		echo "</div>";
 		
 		
