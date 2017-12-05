a9e08fdf6f19dbbe401f5a9c6a21713ad77d28da
Chris Rose <chrislrose@gmail.com>
2017-12-05 08:15:56 -0500

    Version Update

--- config.php
+++ config.php
@@ -28,7 +28,7 @@
 	$pageorder=4;
 	$pagetitle="Assessments";
 	$description="Create & deliver standards-aligned assessments";
-	$version="1.3.7";
+	$version="1.3.8";
 	$repo="abreio/Abre-Assessments";
 	$pageicon="assessment";
 	$pagepath="assessments";
