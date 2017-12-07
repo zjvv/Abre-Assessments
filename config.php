a9b53283222c1866f9ad0318a353942c9869d3f2
Christopher Rose <crose@hcsdoh.org>
2017-12-07 11:55:46 -0500

    Updated Version

--- config.php
+++ config.php
@@ -28,7 +28,7 @@
 	$pageorder=4;
 	$pagetitle="Assessments";
 	$description="Create & deliver standards-aligned assessments";
-	$version="1.3.8";
+	$version="1.3.9";
 	$repo="abreio/Abre-Assessments";
 	$pageicon="assessment";
 	$pagepath="assessments";
